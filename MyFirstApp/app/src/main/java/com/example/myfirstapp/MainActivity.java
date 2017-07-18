6a4322697edcd42bd4a80152f21e1727d4bebb2b
Fatih AKSEL <fatih@Fatihs-MacBook-Pro.local>
2017-07-18 15:45:27 +0300

    onSendClick added

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSendClick(View view) {
+        Toast.makeText(getApplicationContext(), R.string.toast_message, Toast.LENGTH_LONG).show();
     }
 }
